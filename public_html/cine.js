e77e516e7147a8a542a7c0573321eb784405da1a
matiasdaw2015 <matiastalaya@gmail.com>
2015-03-04 13:37:04 +0100

    PrimerCommit

---
+++ public_html/cine.js
@@ -0,0 +1,42 @@
+//
+//function cargarpagina(){
+//    document.getElementById("login").style.display="block"; 
+//    document.getElementById("taquilla").style.display="none"; 
+//}
+//
+//function irpagina(paginanueva){
+//  var secciones;
+//  var numeroseccion;
+//  var seccionvisible;
+//  
+//  secciones=document.getElementsByTagName("section");
+//  for (numeroseccion=0;numeroseccion<secciones.length;numeroseccion++)
+//  {
+//      document.getElementById(secciones[numeroseccion].id).style.display="none";
+//  };
+//  
+//  switch (paginanueva) { 
+//   	case "taquilla": 
+//      	seccionvisible="taquilla"; 
+//      	break 
+//   	case "especies": 
+//      	seccionvisible="taquilla"; 
+//      	break 
+//   	case "reservas": 
+//      	seccionvisible="terceraseccion"; 
+//      	break 
+//   	case "galeria": 
+//      	seccionvisible="cuartaseccion"; 
+//      	break 
+//};
+// document.getElementById(seccionvisible).style.display="block";  
+//};
+//
+//function comprobarlogin(nombre,contraseña)
+//{
+//    alert(nombre);
+//    alert(contraseña);
+//}
+
+
+
